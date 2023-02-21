02b0233fc87505f6d8282bde1340c795cf30c079
zainabolayide <zainab100.too@gmail.com>
2023-02-21 13:47:36 -0800

    programming

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-<<<<<<< HEAD
-int _putchar(char);
-=======
-int _putchar(cha)
->>>>>>> be5aea2af406f35c05dbee6db9343abee9ffafa1
+int _putchar(char)
+void print_alphabet(void);
+
+#endif
