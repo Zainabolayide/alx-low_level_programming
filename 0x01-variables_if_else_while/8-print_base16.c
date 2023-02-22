4042392d251eb3d534e2e2fe049020419e57a3a9
zainabolayide <zainab100.too@gmail.com>
2023-02-22 13:11:16 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 	int num;
 	char le;
 
-	for (num = 0; num < 16; num++)
+	for (num = 0; num < 10; num++)
 
-		putchar((num % 16) + '0');
+		putchar((num % 10) + '0');
 
 	for (le = 'a'; le <= 'f'; le++)
 
