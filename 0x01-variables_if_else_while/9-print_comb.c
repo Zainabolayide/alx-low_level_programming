2ab9429854d4dca3bf401d4011fb86ba9d272a40
zainabolayide <zainab100.too@gmail.com>
2023-02-23 09:35:32 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,14 @@ int main(void)
 	int n;
 
 	for (n = 48; n  <= 57; n++)
-	{
-	if (n == 57)
-	{
-		continue;
-	}
+{
 		putchar(n);
+
+if (n == 57)
+{
+continue;
+
+}
 		putchar(',');
 		putchar(' ');
 		}
